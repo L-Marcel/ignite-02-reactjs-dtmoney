3a4d240ece4aa996645984fc6511fb4773ce6a4f
LMarcel <lmgh1312@gmail.com>
2021-11-04 11:06:38 -0300

    Estrutura da aplicação

---
+++ src/styles/global.ts
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f0f2f5;
+    --red: #E52e54;
+    --blue: #5429CC;
+    --green: #33CC95;
+
+    --blue-light: #6933FF;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --shape: #FFFFFF;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1800px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(--background);
+    --webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`;
